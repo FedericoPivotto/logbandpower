3989a5e4e116aea37f41fb1f98a4c6dc46b87bbb
Federico Pivotto <federico.pivotto@studenti.unipd.it>
2025-01-22 22:08:43 +0100

    Callbacks

--- scripts/record.py
+++ scripts/record.py
@@ -2,22 +2,43 @@
 import rospy, rosbag
 from rosneuro_msgs.msg import NeuroFrame, NeuroEvent
 
-def eeg_filtered_callback():
-	pass
+def eeg_filtered_callback(data: NeuroFrame):
+	# ROS bag
+	global bag, eeg_filtered
+	try:
+		# Write bag
+		bag.write(eeg_filtered, data)
+	except:
+		# Close bag
+		bag.close()
 
-def eeg_bandpower_callback():
-	pass
+def eeg_bandpower_callback(data: NeuroFrame):
+	# ROS bag
+	global bag, eeg_bandpower
+	try:
+		# Write bag
+		bag.write(eeg_bandpower, data)
+	except:
+		# Close bag
+		bag.close()
 
-def events_bus_callback():
-	pass
+def events_bus_callback(data: NeuroEvent):
+	# ROS bag
+	global bag, events_bus
+	try:
+		# Write bag
+		bag.write(events_bus, data)
+	except:
+		# Close bag
+		bag.close()
 
 def main():
 	# Init the node
 	rospy.init_node('record')
 	
 	# ROS bag
-	global bag
-	bag = rosbag.Bag('result.bag', 'w')
+	global bag, eeg_filtered, eeg_bandpower, events_bus
+	bag = rosbag.Bag('../record/result.bag', 'w')
 
     # Topics to record
 	eeg_filtered = rospy.get_param('eeg_filtered', 'eeg/filtered')
